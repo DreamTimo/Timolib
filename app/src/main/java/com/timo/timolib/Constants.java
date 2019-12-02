4dfc902820cc058c1b93506a6ec48dec0b0e5c4e
caiyongwang <dreamtimo@163.com>
2019-12-02 11:19:51 +0800

    init

--- app/src/main/java/com/timo/timolib/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.timo.timolib;
-
-/**
- * Created by 蔡永汪 on 2019/8/9.
- */
-
-public interface Constants {
-    String title_home = "title_home";
-}
--- app/src/main/java/com/timo/timolib/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/timo/timolib/ui/activity/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.timo.timolib.ui.activity.main
 
+import com.eidlink.facesilent.FaceSdkManager
+import com.eidlink.facesilent.inter.OnGetResultListener
 import com.timo.timolib.R
 import com.timo.timolib.mvp.MVPBaseActivity
 
@@ -12,5 +14,14 @@ import com.timo.timolib.mvp.MVPBaseActivity
 class MainActivity : MVPBaseActivity<MainContract.View, MainPresenter>(), MainContract.View {
     override fun getContentResId(): Int = R.layout.activity_main
     override fun initEvent() {
+        FaceSdkManager.start(this, object : OnGetResultListener {
+            override fun onSuccess(p0: String?) {
+                showToast(p0)
+            }
+
+            override fun onFailed(p0: String?, p1: String?) {
+                showToast(p0)
+            }
+        })
     }
 }
